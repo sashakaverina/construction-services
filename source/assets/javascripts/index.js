69076ee5eb639ffa2ab3f8c627622e98cc638f6d
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-10-12 18:00:44 +0900

    fixed counter js

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,30 +1,47 @@
 const counters = document.querySelectorAll('.counter');
 const speed = 12; // The lower the slower
 
-counters.forEach(counter => {
-	const updateCount = () => {
-		const target = +counter.getAttribute('data-target');
-		const count = +counter.innerText;
+const isInViewport = function (elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
-		// Lower inc to slow and higher to slow
-		const inc = target / speed;
+const counterContainer = document.querySelector('.counter-container');
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(counterContainer)) {
+		counters.forEach(counter => {
+			const updateCount = () => {
+				const target = +counter.getAttribute('data-target');
+				const count = +counter.innerText;
+		
+				// Lower inc to slow and higher to slow
+				const inc = target / speed;
+		
+				// console.log(inc);
+				// console.log(count);
+		
+				// Check if target is reached
+				if (count < target) {
+					// Add inc to count and output in counter
+					counter.innerText = count + inc;
+					// Call function every ms
+					setTimeout(updateCount, 100);
+				} else {
+					counter.innerText = target;
+				}
+			};
+		
+			updateCount();
+		});
+	}
+}, false);
 
-		// console.log(inc);
-		// console.log(count);
 
-		// Check if target is reached
-		if (count < target) {
-			// Add inc to count and output in counter
-			counter.innerText = count + inc;
-			// Call function every ms
-			setTimeout(updateCount, 100);
-		} else {
-			counter.innerText = target;
-		}
-	};
-
-	updateCount();
-});
 
 const navbarItems = document.querySelectorAll(".navbar-tabs-item");
 
